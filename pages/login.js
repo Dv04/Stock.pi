979b3371d876b2324eb9e9bf2572768d257d8274
naimish <62000943+naimish73@users.noreply.github.com>
2023-02-04 01:38:13 +0530

    page structure finished

---
+++ pages/login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from "./../styles/login.module.scss";
+
+function login() {
+  return (
+    <div className={styles.login}>
+        <div className={styles.yellow}>this is login page ...</div>
+    </div>
+  )
+}
+
+export default login;
